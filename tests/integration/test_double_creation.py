dc49189912df5c24e229400ae3c125f472e4598a
ryan <hendersonry@gmail.com>
2016-09-23 15:31:17 +0200

    fixture

--- tests/integration/test_double_creation.py
+++ tests/integration/test_double_creation.py
@@ -3,6 +3,26 @@ import pytest
 import rethinkdb as r
 
 
+@pytest.fixture
+def inputs(b, user_vk):
+    # 1. create blocks with transactions for `USER` to spend
+    prev_block_id = b.get_last_voted_block()['id']
+    for block in range(4):
+        transactions = []
+        for i in range(10):
+            tx = b.create_transaction(b.me, user_vk, None, 'CREATE')
+            tx_signed = b.sign_transaction(tx, b.me_private)
+            transactions.append(tx_signed)
+
+        block = b.create_block(transactions)
+        b.write_block(block, durability='hard')
+
+        # 2. vote the blocks valid, so that the inputs are valid
+        vote = b.vote(block['id'], prev_block_id, True)
+        prev_block_id = block['id']
+        b.write_vote(vote)
+
+
 @pytest.mark.usefixtures('processes')
 def test_fast_double_create(b, user_vk):
     tx = b.create_transaction(b.me, user_vk, None, 'CREATE')
